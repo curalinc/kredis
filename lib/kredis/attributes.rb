b53ff5f62d450833621728a882f324a705d6a704
Adam Miribyan <321133+adammiribyan@users.noreply.github.com>
2023-09-08 12:45:58 +0200

    Add kredis_cycle

--- lib/kredis/attributes.rb
+++ lib/kredis/attributes.rb
@@ -72,6 +72,10 @@ module Kredis::Attributes
       kredis_connection_with __method__, name, key, default: default, config: config, after_change: after_change, expires_in: expires_in
     end
 
+    def kredis_cycle(name, values:, key: nil, default: nil, config: :shared, after_change: nil, expires_in: nil)
+      kredis_connection_with __method__, name, key, values: values, default: default, config: config, after_change: after_change, expires_in: expires_in
+    end
+
     def kredis_hash(name, key: nil, default: nil, typed: :string, config: :shared, after_change: nil)
       kredis_connection_with __method__, name, key, default: default, typed: typed, config: config, after_change: after_change
     end
